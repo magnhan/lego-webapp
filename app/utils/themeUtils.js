a828a3b9cc7e3b79d593b8e9b63de0364cb3aea7
AdrianAndersen <adrianandersen@protonmail.com>
2020-02-13 20:39:04 +0100

    Remove test mode

---
+++ app/utils/themeUtils.js
@@ -0,0 +1,29 @@
+import imageLogoLight from 'app/assets/logo-dark.png';
+import imageLogoDark from 'app/assets/logo.png';
+
+// Notification bell
+// Border / shadow på lag nytt arrangement
+// Fikse kontrast på påmeldingsantall
+// Lys farge Info om Abakus
+
+export const applySelectedTheme = theme => {
+  global.dispatchEvent(new Event('themeChange'));
+  document.documentElement.setAttribute('data-theme', theme);
+};
+
+export const getTheme = () => {
+  return (
+    (document &&
+      document.documentElement &&
+      document.documentElement.getAttribute('data-theme')) ||
+    'light'
+  );
+};
+
+export const getLogoImage = () => {
+  return getTheme() !== 'dark' ? imageLogoLight : imageLogoDark;
+};
+
+export const getFancyNodeColor = () => {
+  return getTheme() !== 'dark' ? 'rgba(0,0,0, 0.3)' : 'rgba(255,255,255, 0.4)';
+};
